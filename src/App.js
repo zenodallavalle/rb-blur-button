458e2ba13db100a9b01343cd767b0f75fe3a0b93
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-05-30 10:30:54 +0200

    change example page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import * as rb from 'react-bootstrap';
 
 import Button from './lib/components/Button';
 
@@ -13,12 +14,20 @@ function App() {
         <div></div>
         <div>
           <span className='p-2'>
-            <a href='https://www.npmjs.com/package/rb-blur-button'>
+            <a
+              href='https://www.npmjs.com/package/rb-blur-button'
+              target='_blank'
+              rel='noreferrer'
+            >
               <img src={npmIcon} alt='npm' style={{ height: 35 }}></img>
             </a>
           </span>
           <span className='p-2'>
-            <a href='https://github.com/zenodallavalle/rb-blur-button'>
+            <a
+              href='https://github.com/zenodallavalle/rb-blur-button'
+              target='_blank'
+              rel='noreferrer'
+            >
               <img src={gitHubIcon} alt='npm' style={{ height: 35 }}></img>
             </a>
           </span>
@@ -28,28 +37,47 @@ function App() {
         <h4 className='display-4'>React Bootstrap Blur Button</h4>
         <hr></hr>
         <p>
-          The current count is <span>{count}</span>
+          The current count is{' '}
+          <rb.Badge pill variant='dark'>
+            {count}
+          </rb.Badge>
         </p>
-        <p>This is the componenet, try to Click me!</p>
-        <p>
-          <Button onClick={(e) => setCount((c) => c + 1)} className='px-3'>
-            Increment
-          </Button>
-        </p>
-        {count > 0 && (
+
+        <div className='d-flex justify-content-around'>
           <div>
-            <p>As you can see I'll instantly blur</p>
             <p>
-              <Button
-                onClick={(e) => setCount(0)}
-                variant='danger'
-                size='sm'
-                className='px-3'
-              >
-                Reset
-              </Button>
+              This is <span className='text-primary'>rb-blur-button</span>, try
+              to click me!
             </p>
+            <Button
+              onClick={(e) => setCount((c) => c + 1)}
+              className='px-3 mx-1'
+            >
+              Increment with automatic blur
+            </Button>
+            {count > 0 && <p>As you can see I'll instantly blur</p>}
           </div>
+          <div>
+            <p> This is the normal react-bootstrap component</p>
+            <rb.Button
+              onClick={(e) => setCount((c) => c + 1)}
+              className='px-3 mx-1'
+            >
+              Increment without automatic blur
+            </rb.Button>
+          </div>
+        </div>
+        {count > 0 && (
+          <p>
+            <Button
+              onClick={(e) => setCount(0)}
+              variant='danger'
+              size='sm'
+              className='px-3'
+            >
+              Reset
+            </Button>
+          </p>
         )}
       </div>
     </div>
